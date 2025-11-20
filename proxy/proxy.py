6aadeba8d367d415e53162a14d6bc3372d31b22f
Abdul Waheed <68082996+abdulwaheed76@users.noreply.github.com>
2025-11-21 03:16:10 +0500

    Update proxy.py

--- proxy/proxy.py
+++ proxy/proxy.py
@@ -43,4 +43,4 @@ async def proxy(request: Request, full_path: str):
 
 if __name__ == "__main__":
     import uvicorn
-    uvicorn.run(app, host="0.0.0.0", port=3004)
+    uvicorn.run(app, host="0.0.0.0", port=8080)
